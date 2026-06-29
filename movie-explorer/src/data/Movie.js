2f8bcbbca37aec89e14cd135a4c7d98c5f840860
kishan-1285 <kishanragunathan26@gmail.com>
2026-06-29 15:30:53 +0530

    first task

---
+++ movie-explorer/src/data/Movie.js
@@ -0,0 +1,25 @@
+const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    year: 2010,
+    rating: 8.8,
+    poster: "https://example.com/inception.jpg"
+  },
+  {
+    id: 2,
+    title: "The Matrix",
+    year: 1999,
+    rating: 8.7,
+    poster: "https://example.com/the-matrix.jpg"
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    year: 2014,
+    rating: 8.6,
+    poster: "https://example.com/interstellar.jpg"
+  }
+];
+
+export default movies;
